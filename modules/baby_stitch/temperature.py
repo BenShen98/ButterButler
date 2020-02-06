08ee6656fa1e6c15a0d9d7fe4e35e8ffab0821b2
ben <15164268+BenShen98@users.noreply.github.com>
2020-02-06 18:59:14 +0000

    baby_stitch: create daemon

--- modules/baby_stitch/temperature.py
+++ modules/baby_stitch/temperature.py
@@ -1,35 +1,43 @@
 
 import smbus
-import time
-bus = smbus.SMBus(1)
+from time import sleep
+import json
 
+def main(mqtt,idstr):
+    sensordict={0x03:'floortemp', 0x01:'ceilingtemp'} # 0x03:objtemp, 0x01:devicetemp
 
-data = []
-data.append(0x15)
-data.append(0x40)
-bus.write_i2c_block_data(0x40, 0x02, data)
-datal = []
-datal.append(0x15)
-datal.append(0x15)
-bus.write_i2c_block_data(0x40, 0x02, datal)
-index =0
-while(True):
-    data = bus.read_i2c_block_data(0x40, 0x03, 2)
-    datal = bus.read_i2c_block_data(0x40, 0x01, 2)
-    # Convert object temperature to 14-bits
-    cTemp = ((data[0] * 256 + (data[1] & 0xFC)) / 4)
-    if cTemp > 8191 :
-        cTemp -= 16384
-    cTemp = cTemp * 0.03125
-    #convert die temperature to 14-bits
-    cTempl = ((datal[0] * 256 + (datal[1] & 0xFC)) / 4)
-    if cTempl > 8191 :
-        cTempl -= 16384
-    cTempl = cTempl * 0.03125
+    # register to server
+    for sensorname in sensordict.values():
+        config = json.dumps({"device_class": "temperature",
+            "name": sensorname,
+            "state_topic": f"butterbutler/sensor/{idstr}",
+            "unit_of_measurement": "°C",
+            "value_template": "{{ value_json."+sensorname+"}}"})
 
+        mqtt.publish(f"homeassistant/sensor/{idstr}_{sensorname}/config",config)
 
-    print(f'Object Temperature in Celsius :{cTemp} C')
-    print(f'local Temperature in Celsius :{cTempl} C')
+    # init sensor
+    bus = smbus.SMBus(1)
+    data = []
+    data.append(0x15)
+    data.append(0x40)
+    bus.write_i2c_block_data(0x40, 0x02, data)
+    datal = []
+    datal.append(0x15)
+    datal.append(0x15)
+    bus.write_i2c_block_data(0x40, 0x02, datal)
 
+    # read data
+    reading={}
+    while(True):
+        for sensoraddr,sensorname in sensordict.items():
+            data = bus.read_i2c_block_data(0x40, sensoraddr, 2)
+            # Convert temperature to 14-bits
+            cTemp = ((data[0] * 256 + (data[1] & 0xFC)) / 4)
+            if cTemp > 8191 :
+                cTemp -= 16384
+            cTemp = cTemp * 0.03125
+            reading[sensorname]=cTemp
 
-
+        mqtt.publish(f"butterbutler/sensor/{idstr}", json.dumps(reading))
+        sleep(1)
